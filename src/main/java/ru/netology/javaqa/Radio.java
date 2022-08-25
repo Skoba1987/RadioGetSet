c28a5e591ab98d9ce59995ab81a3832cc2436062
Skoba Kristina <105549745+Skoba1987@users.noreply.github.com>
2022-08-25 20:26:44 +0300

    Update Radio.java

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Radio {
+package ru.netology.javaqa;
+
+public class Radio {
+    public int currentVolume;
+    public int currentStation;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+            currentStation = currentStation - 1;
+        }
+    }
+
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 9) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void next() {
+        if (currentVolume == 9) {
+            currentVolume = 0;
+        } else {
+            currentVolume++;
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void prev() {
+        if (currentVolume == 0) {
+            currentVolume = 9;
+        } else {
+            currentVolume--;
+            currentVolume = currentVolume - 1;
+        }
+    }
 }
