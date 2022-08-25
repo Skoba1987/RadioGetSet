30fbc8f8663f5eb1909431bd637d5c549ff85f4a
Skoba Kristina <kfisher@mail.ru>
2022-08-25 21:52:44 +0300

    new branch

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class Radio {
+package ru.netology.javaqa;
+
+public class Radio {
+    public int currentVolume;
+    public int currentStation;
+    public int stationAmount;
+
+    public Radio(){
+        stationAmount = 10;
+    }
+    public Radio(int stationAmount) {
+        stationAmount = 10;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getCurrentStation(){
+
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > stationAmount - 1) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void next() {
+        if (currentStation == stationAmount - 1) {
+            currentStation = 0;
+        } else {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = stationAmount - 1;
+        } else {
+            currentStation = currentStation - 1;
+        }
+    }
+
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > stationAmount - 1) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 10) {
+            currentVolume = currentVolume - 1;
+        }
+    }
 }
--- src/test/java/ru/netology/javaqa/RadioTest.java
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class RadioTest {
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void testNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(7);
+        radio.next();
+
+        int expected = 8;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void testPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        radio.prev();
+
+        int expected = 4;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void testPrevVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(5);
+        radio.prev();
+
+        int expected = 4;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void testNextVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(7);
+        radio.next();
+
+        int expected = 8;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
